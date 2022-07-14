7b7431efc49a0a48871c1f107e7bb3dedefafbfb
J3DEL <98067906+J3DEL@users.noreply.github.com>
2022-07-14 15:59:45 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function login(){
+    var user, pass;
+
+    user = document.getElementById("usuario").value;
+    pass = document.getElementById("contraseña").value;
+
+    if(user == 'Usuario@correo.com' && pass == '1234'){
+        window.location= "../html/Sesion.html";
+        alert('Se ha iniciado sesion con exito');
+    }
+}
+
+let Ingresar = document.getElementById('ejecutar');
+Ingresar.addEventListener('click', login);
