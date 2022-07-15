6cc40a946d57de8d6cfb62e0a56719f4e6780acd
JimmyVin <jimmy.dragon1@hotmail.com>
2022-07-15 13:10:16 -0500

    validando registro

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-var usuarios = [];
-var contrasenias = [];
 
-function registro() {
-    
-}
-
-function login(){
+let Ingresar = document.getElementById('ejecutar');
+Ingresar.addEventListener('click', function(){
     var user, pass;
 
     user = document.getElementById("usuario").value;
     pass = document.getElementById("contraseña").value;
 
     if(user == 'Usuario@correo.com' && pass == '1234'){
-        location.href="../html/index-menu.html";
+        
+        location.href='./html/index-menu.html';
         alert('Se ha iniciado sesion con exito');
+        
     }
-}
+});
 
-let Ingresar = document.getElementById('ejecutar');
-Ingresar.addEventListener('click', login);
+let Registrar = document.getElementById('registro');
+Registrar.addEventListener('click', function(){
+    alert('registrado satisfactoriamente');
+    location.href='../index.html';
+    /*
+    var user, pass;
+
+    user = document.getElementById("usuario").value;
+    pass = document.getElementById("contraseña").value;
+
+    if(user == 'Usuario@correo.com' && pass == '1234'){
+        
+        location.href='../index.html';
+        alert('Se ha iniciado sesion con exito');
+        
+    }
+    */
+});
